c62bb9d7a86edafaa032525cec01bc191d82324b
Nicole MacLean <nicole.mac0404@gmail.com>
2022-07-18 18:03:16 -0700

    Modularise profile avatar

--- my-profile/src/App.js
+++ my-profile/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import ProfileAvatar from "./components/avatar";
+
 import Projects from "./components/projects";
 import Contact from "./components/contact";
 import Email from "./components/email";
 import Skills from "./components/skills";
 
 import { Center, Text, Box, Divider, Grid, GridItem } from "@chakra-ui/react";
+import Profile from "./profile";
 
 function App() {
   return (
@@ -18,40 +19,7 @@ function App() {
         padding={5}
       >
         <GridItem colSpan={1} bg="transparent">
-          <Box backgroundColor="brand.600" padding={4}>
-            <Center flexDirection="column">
-              <ProfileAvatar />
-
-              <Text alignItems="flex-start" fontSize="xl">
-                Nicole MacLean{" "}
-              </Text>
-              <Divider />
-              <Text alignItems="flex-start" fontSize="sm">
-                Full Stack Developer.
-              </Text>
-              <br />
-              <br />
-              <br />
-              <br />
-              <Text fontSize="xs" as="em">
-                "There is no royal road to anything, one thing at a time, all
-                things in succession. That which grows fast, withers as rapidly.
-                That which grows slowly, endures."
-              </Text>
-              <Text fontSize="xs">- Josiah Gilbert Hollandd</Text>
-            </Center>
-            <br />
-            <br />
-            <br />
-            <br />
-            <Box>
-              <Contact></Contact>
-            </Box>
-          </Box>
-          <br />
-          <br />
-          <br />
-          <br />
+          <Profile></Profile>
         </GridItem>
 
         <GridItem colSpan={4} bg="transparent">
